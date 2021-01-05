ef47f747107aa09f93c1c8468a2bd0851b1263e0
Shreshth Mohan <sremog@gmail.com>
2021-01-05 13:57:15 +0530

    add file with list of spec options in dropdowns

---
+++ src/specificationOptions.js
@@ -0,0 +1,11 @@
+const tapping_switch = ['cicular', 'linear', 'online']
+const location = ['outdoor', 'indoor']
+const winding_conductor = ['aluminium', 'copper'] // TODO: Allow HT and LT conductor metals to be different?
+const connections = ['delta', 'star', 'zigzag'] // TODO: zigzag?
+const tank_shape = ['rectangular', 'oval']
+const radiator_type = ['pressed', 'elleptical', 'corrugated']
+const insulation_type = [
+  'double paper covering',
+  'triple paper covering',
+  'super enamelled',
+]
