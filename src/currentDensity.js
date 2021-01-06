152d71eefc949cb685928c546890e5e54517ef2a
Shreshth Mohan <sremog@gmail.com>
2021-01-06 20:40:40 +0530

    New function to decide current density

---
+++ src/currentDensity.js
@@ -0,0 +1,20 @@
+const COPPER = 'copper'
+const ALUMINIUM = 'aluminium'
+const currentDensity = function (ht_conductor_material, lt_conductor_material) {
+  let ht_current_density // A/mm²
+  let lt_current_density // A/mm²
+  if (ht_conductor_material === COPPER) {
+    ht_current_density = 3.2
+  }
+  if (ht_conductor_material === ALUMINIUM) {
+    ht_current_density = 1.6
+  }
+  if (lt_conductor_material === COPPER) {
+    lt_current_density = 3.5
+  }
+  if (lt_conductor_material === ALUMINIUM) {
+    lt_current_density = 1.7
+  }
+
+  return { lt_current_density, ht_current_density }
+}
