5d85bb282fb791ba9739e0ad8563a3f284208a63
Shreshth Mohan <sremog@gmail.com>
2021-01-09 15:48:06 +0530

    function for LT turns done

--- ltTurns.js
+++ ltTurns.js
@@ -1,5 +1,6 @@
 const ALUMINIUM = 'aluminium'
 const COPPER = 'copper'
+const ZIGZAG = 'zigzag'
 const ltTurns = function ({
   power_rating,
   ht_nominal_voltage,
@@ -8,199 +9,163 @@ const ltTurns = function ({
   cost_factor,
   lt_voltage_per_leg,
   percentage_impedance,
+  lt_connection,
 }) {
+  // TODO: Error generation
+
   let cost_impedance_factor
   let x // Ampere^-1
   let lt_turns
-  if (ht_conductor_material === ALUMINIUM) {
-    cost_impedance_factor = cost_factor * Math.sqrt(percentage_impedance / 4.5)
-    if (power_rating < 63) {
-      if (cost_impedance_factor < 1.1) {
-        x = 0.316
-      } else if (cost_impedance_factor >= 1.1 && cost_impedance_factor < 1.4) {
-        x = 0.308
+  if (ht_nominal_voltage < 13000) {
+    if (ht_conductor_material === ALUMINIUM) {
+      //   CASE CTYPE#'A' .AND. NOMHTV<13000
+      cost_impedance_factor =
+        cost_factor * Math.sqrt(percentage_impedance / 4.5)
+      if (power_rating < 63) {
+        if (cost_impedance_factor < 1.1) {
+          x = 0.316
+        } else if (
+          cost_impedance_factor >= 1.1 &&
+          cost_impedance_factor < 1.4
+        ) {
+          x = 0.308
+        } else {
+          x = 0.3
+        }
+        lt_turns = lt_voltage_per_leg / (x * Math.sqrt(power_rating))
+        lt_turns = 2 * Math.round(lt_turns / 2)
+      } else if (power_rating < 100) {
+        if (cost_impedance_factor < 1.1) {
+          x = 0.34
+        } else if (
+          cost_impedance_factor >= 1.1 &&
+          cost_impedance_factor < 1.4
+        ) {
+          x = 0.325
+        } else {
+          x = 0.315
+        }
+        lt_turns = Math.round(lt_voltage_per_leg / x / Math.sqrt(power_rating))
+      } else if (power_rating < 200) {
+        if (cost_impedance_factor < 1.1) {
+          x = 0.35
+        } else if (
+          cost_impedance_factor >= 1.1 &&
+          cost_impedance_factor < 1.4
+        ) {
+          x = 0.33
+        } else {
+          x = 0.315
+        }
+        lt_turns = Math.round(lt_voltage_per_leg / x / Math.sqrt(power_rating))
       } else {
-        x = 0.3
+        // power_rating >= 200
+        if (cost_impedance_factor < 1.1) {
+          x = 0.34
+        } else if (
+          cost_impedance_factor >= 1.1 &&
+          cost_impedance_factor < 1.4
+        ) {
+          x = 0.32
+        } else {
+          x = 0.305
+        }
+        lt_turns = Math.round(lt_voltage_per_leg / x / Math.sqrt(power_rating))
       }
-      lt_turns = lt_voltage_per_leg / (x * Math.sqrt(power_rating))
-      lt_turns = 2 * Math.floor(lt_turns / 2 + 0.5)
-    } else if (power_rating < 100) {
-      if (cost_impedance_factor < 1.1) {
-        x = 0.34
-      } else if (cost_impedance_factor >= 1.1 && cost_impedance_factor < 1.4) {
-        x = 0.325
+    } else if (ht_conductor_material === COPPER) {
+      if (power_rating <= 200) {
+        cost_impedance_factor =
+          cost_factor * Math.sqrt(percentage_impedance / 4.5)
+        if (cost_impedance_factor < 0.7) {
+          x = 0.38
+        } else if (cost_impedance_factor < 0.9) {
+          x = 0.36
+        } else {
+          x = 0.35
+        }
+      } else if (power_rating <= 500) {
+        cost_impedance_factor =
+          cost_factor * Math.sqrt(percentage_impedance / 4.65)
+        if (cost_impedance_factor < 0.7) {
+          x = 0.39
+        } else if (cost_impedance_factor < 0.9) {
+          x = 0.37
+        } else {
+          x = 0.35
+        }
       } else {
-        x = 0.315
+        // power_rating > 500
+        cost_impedance_factor =
+          cost_factor * Math.sqrt(percentage_impedance / 5)
+        if (cost_impedance_factor < 0.7) {
+          x = 0.42
+        } else if (cost_impedance_factor < 0.9) {
+          x = 0.39
+        } else {
+          x = 0.37
+        }
       }
-      lt_turns = Math.floor(
-        lt_voltage_per_leg / x / Math.sqrt(power_rating) + 0.5
-      )
-      //  TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
+      lt_turns = Math.round(lt_voltage_per_leg / x / Math.sqrt(power_rating))
+      if (power_rating > 250 && short_circuit_test) {
+        lt_turns = lt_turns - 1
+      }
+    }
+  } else {
+    // You won't normally have a transformer with ht voltage above 13kv and conductor material aluminium. Reason? Size?
+    // ht_nominal_voltage >= 13000
+    if (power_rating < 200) {
+      cost_impedance_factor =
+        cost_factor * Math.sqrt(percentage_impedance / 4.75)
+      if (cost_impedance_factor < 0.7) {
+        x = 0.38
+      } else if (cost_impedance_factor < 0.9) {
+        x = 0.365
+      } else {
+        x = 0.347
+      }
+    } else if (power_rating <= 500) {
+      cost_impedance_factor = cost_factor * Math.sqrt(percentage_impedance / 5)
+      if (cost_impedance_factor < 0.7) {
+        x = 0.42
+      } else if (cost_impedance_factor < 0.9) {
+        x = 0.38
+      } else {
+        x = 0.36
+      }
+    } else if (power_rating <= 1600) {
+      cost_impedance_factor =
+        cost_factor * Math.sqrt(percentage_impedance / 5.5)
+      if (cost_impedance_factor < 0.7) {
+        x = 0.44
+      } else if (cost_impedance_factor < 0.9) {
+        x = 0.42
+      } else {
+        x = 0.395
+      }
+    } else {
+      // power_rating > 1600
+      if (power_rating < 5000) {
+        cost_impedance_factor =
+          cost_factor * Math.sqrt(percentage_impedance / 6.25)
+      } else {
+        cost_impedance_factor =
+          cost_factor * Math.sqrt(percentage_impedance / 7.5)
+      }
+      if (cost_impedance_factor < 0.7) {
+        x = 0.44
+      } else if (cost_impedance_factor < 0.9) {
+        x = 0.42
+      } else {
+        x = 0.395
+      }
+    }
+    lt_turns = Math.round(lt_voltage_per_leg / x / Math.sqrt(power_rating))
+    if (power_rating > 250 && short_circuit_test) {
+      lt_turns = lt_turns - 1
     }
   }
+  if (lt_connection === ZIGZAG) {
+    lt_turns *= 2 / Math.sqrt(3)
+  }
+  return lt_turns
 }
-
-// PARAMETER KVA,NOMHTV,SCTEST,LVLEG,CTYPE,COSTFACT,TURNLT
-// DO CASE
-//   CASE CTYPE='A'
-//     FACT=COSTFACT*SQRT(Z/4.500)
-//     IF KVA<63
-//       IF FACT < 1.1
-//         X=0.316
-//       ELSE
-//         IF FACT>= 1.1 .AND. FACT<1.4
-//           X=0.308
-//         ELSE
-//           X=0.3
-//         ENDIF
-//       ENDIF
-//       TURNLT=LVLEG/(X*SQRT(KVA))
-//       TURNLT=2*INT(TURNLT/2.0+0.5)
-//     ENDIF
-//     IF KVA>=63 .AND. KVA<100
-//       IF FACT<1.1
-//         X=0.34
-//       ELSE
-//         IF FACT>= 1.1 .AND. FACT<1.4
-//           X=0.325
-//         ELSE
-//           X=0.315
-//         ENDIF
-//       ENDIF
-//       TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
-//     ENDIF
-//     IF KVA>= 100 .AND. KVA<200
-//       IF FACT<1.1
-//         X=0.35
-//       ELSE
-//         IF FACT>=1.1 .AND. FACT<1.4
-//           X=0.33
-//         ELSE
-//           X=0.315
-//         ENDIF
-//       ENDIF
-//       TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
-//     ENDIF
-//     IF KVA>=200
-//       IF FACT<1.1
-//         X=0.34
-//       ELSE
-//         IF FACT>=1.1 .AND. FACT<1.4
-//           X=0.32
-//         ELSE
-//           X=0.305
-//         ENDIF
-//       ENDIF
-//       TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
-//     ENDIF
-//   CASE CTYPE#'A' .AND. NOMHTV<13000
-// * CU CONDUCTOR BELOW 13KV CLASS T/F.
-//     IF KVA<=200
-//       FACT=COSTFACT*SQRT(Z/4.5)
-//       IF FACT<0.7
-//         X=0.38
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<.9
-//           X=0.36
-//         ELSE
-//           X=0.35
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     IF KVA>200 .AND. KVA<=500
-//       FACT=COSTFACT*SQRT(Z/4.650)
-//       IF FACT<0.7
-//         X=0.39
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.37
-//         ELSE
-//           X=0.35
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     IF KVA>500
-//       FACT=COSTFACT*SQRT(Z/5.000)
-//       IF FACT<0.7
-//         X=0.42
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.39
-//         ELSE
-//           X=0.37
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
-//     IF KVA>250 .AND. UPPER(SCTEST)='Y'
-//       TURNLT=TURNLT-1
-//     ENDIF
-//   CASE NOMHTV>=13000
-//     IF KVA<200
-//       FACT=COSTFACT*SQRT(Z/4.75)
-//       IF FACT<0.7
-//         X=0.38
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.365
-//         ELSE
-//           X=0.347
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     IF KVA>=200 .AND. KVA<=500
-//       FACT=COSTFACT*SQRT(Z/5.0)
-//       IF FACT<0.7
-//         X=0.42
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.38
-//         ELSE
-//           X=0.36
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     IF KVA>500 .AND. KVA<=1600
-//       FACT=COSTFACT*SQRT(Z/5.5)
-//       IF FACT<0.7
-//         X=0.44
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.42
-//         ELSE
-//           X=0.395
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     IF KVA>1600 .AND. KVA<5000
-//       FACT=COSTFACT*SQRT(Z/6.25)
-//     ELSE
-//       IF KVA>=5000
-//         FACT=COSTFACT*SQRT(Z/7.5)
-//       ENDIF
-//     ENDIF
-//     IF KVA>1600
-//       IF FACT<0.7
-//         X=0.44
-//       ELSE
-//         IF FACT>=0.7 .AND. FACT<0.9
-//           X=0.42
-//         ELSE
-//           X=0.4
-//         ENDIF
-//       ENDIF
-//     ENDIF
-//     TURNLT=INT(LVLEG/X/SQRT(KVA)+0.5)
-//     IF KVA>250 .AND. UPPER(SCTEST)='Y'
-//       TURNLT=TURNLT-1
-//     ENDIF
-//   OTHERWISE
-//     CLEAR
-//     @ 10,10 SAY 'SOME THING WRONG ?'
-//     WAIT
-// ENDCASE
-// IF CONNLT='Z'
-//   TURNLT=TURNLT*2/SQRT(3.000)
-// ENDIF
-// RETURN
-// 
