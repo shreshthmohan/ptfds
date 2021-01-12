9f16907d09577c188b6aa740f29bfd1ed45b5e50
Shreshth Mohan <sremog@gmail.com>
2021-01-12 16:19:32 +0530

    Added two incomplete functions, spiral and winding tolerances.

---
+++ src/spiralWinding.js
@@ -0,0 +1,9 @@
+const spiralWinding = function ({
+  lt_turns,
+  lt_layers,
+  lt_conductor_area,
+  lt_conductor_material,
+  lt_covering_thickness,
+  lt_winding_height,
+  lt_gap,
+}) {}
---
+++ src/windingTolerances.js
@@ -0,0 +1,13 @@
+const END_LEVELLING = [2, 2.5, 3]
+const NORMAL = 'normal'
+const TIGHT = 'tight'
+const VERY_TIGHT = 'very tight'
+const MANUFACTURING_LEVEL = [NORMAL, TIGHT, VERY_TIGHT]
+
+switch (MANUFACTURING_LEVEL) {
+  case value:
+    break
+
+  default:
+    break
+}
